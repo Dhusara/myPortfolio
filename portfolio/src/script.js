b2b817d8e7e53fad34c7fe89f0b70acc86cd01b0
Serge <59541094+Dhusara@users.noreply.github.com>
2020-02-06 21:28:39 +0200

    Update script.js

--- portfolio/src/script.js
+++ portfolio/src/script.js
@@ -35,13 +35,3 @@ function blinker() {
 }
 
 setInterval(blinker, 1000); //Runs every second
-
-// validate email form
-
-var frmvalidator  = new Validator("contactform");
-frmvalidator.addValidation("name","req","Please provide your name");
-frmvalidator.addValidation("email","req","Please provide your email");
-frmvalidator.addValidation("email","email",
-  "Please enter a valid email address");
-
-
